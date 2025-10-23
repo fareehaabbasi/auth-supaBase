1a43aa7802c1fab8941d914f8222427d4dc0b1db
fareehaabbasi <abbasifareeha9@gmail.com>
2025-10-23 20:52:05 +0500

    Login-auth-complete

--- auth.js
+++ auth.js
@@ -7,20 +7,25 @@ let signPhone = document.getElementById("phone");
 let signEmail = document.getElementById("email");
 let signPassword = document.getElementById("password");
 
+// ---------------Loader functions
 const loader = document.getElementById("loader-overlay");
-
 function showLoader() {
-    loader.style.display = 'flex';
-};
+  loader.style.display = "flex";
+}
 function hideLoader() {
-    loader.style.display = 'none';
-};
+  loader.style.display = "none";
+}
 
 //---------Sign-up authentication
 async function signup(e) {
   e.preventDefault();
 
-  if (signEmail.value && signName.value && signPhone.value && signPassword.value) {
+  if (
+    signEmail.value &&
+    signName.value &&
+    signPhone.value &&
+    signPassword.value
+  ) {
     showLoader();
     try {
       const { data, error } = await client.auth.signUp({
@@ -35,7 +40,6 @@ async function signup(e) {
       });
       if (error) throw error;
 
-    
       Swal.fire({
         title: "Sign up successful!",
         text: "Now login with your new account 🎉",
@@ -44,14 +48,21 @@ async function signup(e) {
       }).then(() => {
         window.location.href = "login.html";
       });
-
     } catch (error) {
       console.error(error);
-      Swal.fire({
+      if (signEmail.value === "" || signPassword.value === "") {
+        Swal.fire({
+          icon: "warning",
+          title: "Missing Fields",
+          text: "Please fill all fields",
+        });
+      }else {
+        Swal.fire({
         icon: "error",
-        title: "Signup Failed",
+        title: "signup Failed",
         text: error.message || "Something went wrong 😢",
       });
+      }
     } finally {
       hideLoader();
     }
@@ -64,4 +75,72 @@ async function signup(e) {
   }
 }
 
-signupForm && signupForm.addEventListener('submit', signup);
+signupForm && signupForm.addEventListener("submit", signup);
+
+//---------Login authentication
+let loginForm = document.getElementById("login-form");
+
+let loginEmail = document.getElementById("email");
+let loginPassword = document.getElementById("password");
+
+//---------Sign-up authentication
+async function login(e) {
+  e.preventDefault();
+
+  if (loginEmail.value && loginPassword.value) {
+    showLoader();
+    try {
+      const { data, error } = await client.auth.signInWithPassword({
+        email: loginEmail.value,
+        password: loginPassword.value,
+      });
+      if (error) throw error;
+
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+
+      Toast.fire({
+        icon: "success",
+        title: "Login successful!",
+      });
+
+      setTimeout(() => {
+        window.location.href = "home.html";
+      }, 1500);
+    } catch (error) {
+      console.error(error);
+      if (loginEmail.value === "" || loginPassword.value === "") {
+        Swal.fire({
+          icon: "warning",
+          title: "Missing Fields",
+          text: "Please fill all fields",
+        });
+      }else {
+        Swal.fire({
+        icon: "error",
+        title: "Login Failed",
+        text: error.message || "Something went wrong 😢",
+      });
+      }
+    } finally {
+      hideLoader();
+    }
+  } else {
+    Swal.fire({
+      icon: "warning",
+      title: "Missing Fields",
+      text: "Please fill all fields",
+    });
+  }
+}
+
+loginForm && loginForm.addEventListener("submit", login);
