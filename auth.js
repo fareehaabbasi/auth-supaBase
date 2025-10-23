6390b882630424fa07c5eab21e4660525a185a37
fareehaabbasi <abbasifareeha9@gmail.com>
2025-10-23 18:46:59 +0500

    signup-auth-complete

--- auth.js
+++ auth.js
@@ -1,2 +1,63 @@
-import supabase from "./config";
+import client from "./config.js";
 
+let signupForm = document.getElementById("signup-form");
+
+let signName = document.getElementById("username");
+let signPhone = document.getElementById("phone");
+let signEmail = document.getElementById("email");
+let signPassword = document.getElementById("password");
+
+const loader = document.getElementById("loader-overlay");
+
+function showLoader() {
+    loader.style.display = 'flex';
+};
+function hideLoader() {
+    loader.style.display = 'none';
+};
+
+//---------Sign-up authentication
+async function signup(e) {
+  e.preventDefault();
+
+  if (signEmail.value && signName.value && signPhone.value && signPassword.value) {
+    try {
+      const { data, error } = await client.auth.signUp({
+        email: signEmail.value,
+        password: signPassword.value,
+        options: {
+          data: {
+            Name: signName.value,
+            phone: signPhone.value,
+          },
+        },
+      });
+      if (error) throw error;
+
+      showLoader();
+      Swal.fire({
+        title: "Sign up successful!",
+        text: "Now login with your new account 🎉",
+        icon: "success",
+        confirmButtonText: "OK",
+      }).then(() => {
+        window.location.href = "login.html";
+      });
+    } catch (error) {
+      console.error(error);
+    //     Swal.fire({
+    //     icon: "error",
+    //     title: "Signup Failed",
+    //     text: error.message || "Something went wrong 😢",
+    //   });
+    }
+  } else {
+    Swal.fire({
+      icon: "warning",
+      title: "Missing Fields",
+      text: "Please fill all fields",
+    });
+  }
+}
+
+signupForm && signupForm.addEventListener('submit', signup);
--- config.js
+++ config.js
@@ -1,8 +1,11 @@
-import {createClient} from `https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm`;
-
 const supabaseUrl = 'https://oigwfuzwyxyqpltmjxif.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pZ3dmdXp3eXh5cXBsdG1qeGlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExMzY2MTUsImV4cCI6MjA3NjcxMjYxNX0.RD48P9BT8fZJ77KCqKwkfGZmkaBKOE1XO8RMlg6-h20';
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const { createClient } = supabase;
+const client = createClient(supabaseUrl, supabaseKey);
+console.log(createClient);
+console.log(client);
 
-export default supabase;
+export default client;
+
+
